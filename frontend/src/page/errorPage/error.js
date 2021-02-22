2d04980f6a6a0ffb43899b24f62aef072ef5630e
bachsmile <bachsmile97@gmail.com>
2021-02-22 16:51:05 +0700

    add error page

--- frontend/src/page/errorPage/error.js
+++ frontend/src/page/errorPage/error.js
@@ -1,10 +1,32 @@
-import React, {Component} from 'react'
-
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import HeaderMin from '../../components/header/header_min/headerMin'
+import './error.scss'
 class ErrorPage extends Component {
     render(){
         return(
-            <div>
-                ErrorPage
+            <div className='error'>
+                <HeaderMin/>
+                <div className="detail">
+                    <div className="noFound">
+                        <div className="title">404</div>
+                        <div className="lable">
+                            NOT FOUND PAGE   
+                        </div> 
+                        <div className='back'>
+                            <span>Click back to home
+                                <span className= 'pannelBack'>
+                                    <Link to='/' ><i class="fas fa-home iconHome"></i></Link>
+                                    <div class="nextBtn1">
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                    </div>
+                                </span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
